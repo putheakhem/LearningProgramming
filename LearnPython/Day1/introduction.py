cb9832d958fb4ae4ef32e38d36f71a9cb9dded69
Puthea Khem <puthea.khem@gmail.com>
2017-12-02 20:44:09 +0700

    Learn Python Day1

---
+++ LearnPython/Day1/introduction.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec  2 20:28:49 2017
+
+@author: Puthea
+"""
+
+# Quotation 
+print("Welcome to Python") #double quote for sentennce 
+print('Python3')    # single qoute for word
+print("""This is quote for writing paragraph in python""")
+
+
+# Mutliple Line in Python 
+
+total = 3 + \
+        4 + \
+        5;
+print (total)
---
+++ LearnPython/README.md
@@ -0,0 +1,7 @@
+####################
+Leanr Python Programming
+
+
+# Day 1
+> Introduction to Python Programming and basic syntax of Python
+
