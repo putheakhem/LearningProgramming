46d4e02a03181c6f73417fa6d1a16f768f6a02c1
Puthea Khem <puthea.khem@gmail.com>
2017-12-04 15:05:29 +0700

    Update Day1

--- LearnPython/Day1/introduction.py
+++ LearnPython/Day1/introduction.py
@@ -6,14 +6,38 @@ Created on Sat Dec  2 20:28:49 2017
 """
 
 # Quotation 
-print("Welcome to Python") #double quote for sentennce 
+print("Welcome to Python")#double quote for sentennce 
 print('Python3')    # single qoute for word
-print("""This is quote for writing paragraph in python""")
+print("""This is quote for writing paragraph in 
+      python in first day""")
 
 
 # Mutliple Line in Python 
 
 total = 3 + \
         4 + \
-        5;
+        5 + \
+        4;
 print (total)
+
+# Keyword or Reservered  and, break, if, import 
+
+
+# Line and Identation 
+
+a = 10;
+
+if a >= 10:
+    print(" a is greather than or equal 10");
+else: 
+    print("a is less than 10");
+    
+    
+# Waiting for user 
+
+
+# raw_input  for version 2
+
+input("\n\n  Press any key");
+
+print("Ater waiting your input");
