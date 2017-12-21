6c7a2aa967ff608ddff6e4bf880480a4d9f05f0e
Puthea Khem <puthea.khem@gmail.com>
2017-12-21 21:34:47 +0700

    Restructure Vue.js file

---
+++ VueJSFundamentals/part2/vue.js
@@ -0,0 +1,9 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Welcome to Vue JS Directive',
+    htmlTag: 'Welcome to <small> Vue Js</small>',
+    viewed: true,
+    condition: 'hide', // you can change this value to play with it
+  }
+});
---
+++ VueJSFundamentals/part3/vue.js
@@ -0,0 +1,8 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Welcome to Vue JS Directive',
+    isPrimary: false,
+    isDanger: true
+  }
+});
