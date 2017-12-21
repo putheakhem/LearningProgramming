e39257c6706c3492c282357ec1c3ca2150001da2
Puthea Khem <puthea.khem@gmail.com>
2017-12-21 22:22:25 +0700

    V-for in Vue

---
+++ VueJSFundamentals/part4/vue.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+  el: "#app",
+  data: {
+    menutitle: 'All list',
+    todos: [
+      {text: 'Member', id:1},
+      {text: 'Plugins', id:2},
+      {text: 'Add a Member', id:3}
+    ]
+  }
+
+});
