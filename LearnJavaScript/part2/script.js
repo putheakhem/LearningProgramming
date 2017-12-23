3ec107a76d4a3b11dc0c58c76f8199c568e31993
Puthea Khem <puthea.khem@gmail.com>
2017-12-23 23:27:54 +0700

    javascript syntax

---
+++ LearnJavaScript/part2/script.js
@@ -0,0 +1,28 @@
+// declare variable
+
+var x, y, z;
+
+// Initialze Value to variable
+x = 5;
+y = 6;
+
+// javascript arithmetic operator + - * /
+
+// javascript expression
+
+
+5 * 10;
+x * 10;
+// JavaScript and Camel Case : Hyphens are not allowed in JavaScript such as
+// first-name, last-name, master-card, inter-city
+
+// Underscore
+ var first_name, last_name, master_card, inter_city;
+
+ // Upper Camel Case (Pascal Case)
+
+ var FirstName, LastName, MasterCard, InterCity;
+
+ //Lower Camel Case
+
+ var firstName, lastName, masterCard, interCity;
--- LearnJavaScript/readme.md
+++ LearnJavaScript/readme.md
@@ -1,4 +1,7 @@
 # Learn JavaScript From Zero to Pro
 
-## Part1
+## Part 1
 > Tell how to include JavaScript into html file
+
+## Part 2
+> Basic Syntax of javascript 
